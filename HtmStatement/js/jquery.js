b99d1122599d03f16af1684b9c737d9208750f8e
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-03-26 14:56:20 +0200

    gomb tutituti

--- HtmStatement/js/jquery.js
+++ HtmStatement/js/jquery.js
@@ -12,6 +12,62 @@ $(document).ready(function(){
 
 })
 
+$( "#gc1" )
+  .mouseover(function() {
+    $("#os1").show();
+  })
+  .mouseout(function() {
+    $("#os1").hide();
+  });
+
+$( "#gc2" )
+  .mouseover(function() {
+    $("#os2").show();
+  })
+  .mouseout(function() {
+    $("#os2").hide();
+  });
+
+$( "#gc3" )
+  .mouseover(function() {
+    $("#os3").show();
+  })
+  .mouseout(function() {
+    $("#os3").hide();
+  });
+
+$( "#gc4" )
+  .mouseover(function() {
+    $("#os4").show();
+  })
+  .mouseout(function() {
+    $("#os4").hide();
+  });
+
+$( "#gc5" )
+  .mouseover(function() {
+    $("#os5").show();
+  })
+  .mouseout(function() {
+    $("#os5").hide();
+  });
+
+$( "#gc6" )
+  .mouseover(function() {
+    $("#os6").show();
+  })
+  .mouseout(function() {
+    $("#os6").hide();
+  });
+
+$( "#gc7" )
+  .mouseover(function() {
+    $("#os7").show();
+  })
+  .mouseout(function() {
+    $("#os7").hide();
+  });
+  
 $( "#gc8" )
   .mouseover(function() {
     $("#os8").show();
