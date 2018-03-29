7d365b14b35bbfdd358e9bf6bebde01a435c09f6
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-03-29 15:15:04 +0200

    buttons

--- HtmStatement/js/buttonactions.js
+++ HtmStatement/js/buttonactions.js
@@ -1,7 +1,9 @@
 $( "#lt1" )
   .mouseover(function() {
-    $("#ltbt1").stop().fadeOut('20');   
+    $("#ltbt1").stop().fadeOut('20'); 
+    $("#lthw1").stop().fadeIn('20');  
   })
   .mouseout(function() {
+    $("#lthw1").stop().fadeOut('20'); 
     $("#ltbt1").stop().fadeIn('20');  
   });
