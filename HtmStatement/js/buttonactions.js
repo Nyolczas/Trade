10c5978f9719be97bb93e1758fb94a4017272f43
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-03-29 16:04:02 +0200

    buttons correct

--- HtmStatement/js/buttonactions.js
+++ HtmStatement/js/buttonactions.js
@@ -1,9 +1,79 @@
 $( "#lt1" )
   .mouseover(function() {
-    $("#ltbt1").stop().fadeOut('20'); 
-    $("#lthw1").stop().fadeIn('20');  
+    $("#ltbt1").stop().fadeOut(20); 
+    $("#lthw1").stop().fadeIn(20);  
   })
   .mouseout(function() {
-    $("#lthw1").stop().fadeOut('20'); 
-    $("#ltbt1").stop().fadeIn('20');  
+    $("#lthw1").stop().fadeOut(20); 
+    $("#ltbt1").stop().fadeIn(20);  
+  });
+  
+$( "#lt2" )
+  .mouseover(function() {
+    $("#ltbt2").stop().fadeOut(20); 
+    $("#lthw2").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw2").stop().fadeOut(20); 
+    $("#ltbt2").stop().fadeIn(20);  
+  });
+
+$( "#lt3" )
+  .mouseover(function() {
+    $("#ltbt3").stop().fadeOut(20); 
+    $("#lthw3").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw3").stop().fadeOut(20); 
+    $("#ltbt3").stop().fadeIn(20);  
+  });
+
+$( "#lt4" )
+  .mouseover(function() {
+    $("#ltbt4").stop().fadeOut(20); 
+    $("#lthw4").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw4").stop().fadeOut(20); 
+    $("#ltbt4").stop().fadeIn(20);  
+  });
+
+$( "#lt5" )
+  .mouseover(function() {
+    $("#ltbt5").stop().fadeOut(20); 
+    $("#lthw5").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw5").stop().fadeOut(20); 
+    $("#ltbt5").stop().fadeIn(20);  
+  });
+  
+$( "#lt6" )
+  .mouseover(function() {
+    $("#ltbt6").stop().fadeOut(20); 
+    $("#lthw6").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw6").stop().fadeOut(20); 
+    $("#ltbt6").stop().fadeIn(20);  
+  });
+
+$( "#lt7" )
+  .mouseover(function() {
+    $("#ltbt7").stop().fadeOut(20); 
+    $("#lthw7").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw7").stop().fadeOut(20); 
+    $("#ltbt7").stop().fadeIn(20);  
+  });
+
+$( "#lt8" )
+  .mouseover(function() {
+    $("#ltbt8").stop().fadeOut(20); 
+    $("#lthw8").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#lthw8").stop().fadeOut(20); 
+    $("#ltbt8").stop().fadeIn(20);  
   });
