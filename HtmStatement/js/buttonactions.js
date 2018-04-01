e40c3d276e2d1ef1ee029648646c483f0282f3c6
Nyolczas <nyolczas@gmail.com>
2018-04-01 20:49:31 +0200

    buttons

--- HtmStatement/js/buttonactions.js
+++ HtmStatement/js/buttonactions.js
@@ -12,7 +12,88 @@ $(document).ready(function(){
 	})
 
 })
+// ---------------- aside buttons ----------------
+$( "#gc1" )
+  .mouseover(function() {
+    $("#os1").stop().fadeOut(20); 
+    $("#hw1").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw1").stop().fadeOut(20); 
+    $("#os1").stop().fadeIn(20);  
+  });
+  
+$( "#gc2" )
+  .mouseover(function() {
+    $("#os2").stop().fadeOut(20); 
+    $("#hw2").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw2").stop().fadeOut(20); 
+    $("#os2").stop().fadeIn(20);  
+  });
+  
+$( "#gc3" )
+  .mouseover(function() {
+    $("#os3").stop().fadeOut(20); 
+    $("#hw3").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw3").stop().fadeOut(20); 
+    $("#os3").stop().fadeIn(20);  
+  });
 
+$( "#gc4" )
+  .mouseover(function() {
+    $("#os4").stop().fadeOut(20); 
+    $("#hw4").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw4").stop().fadeOut(20); 
+    $("#os4").stop().fadeIn(20);  
+  });
+
+$( "#gc5" )
+  .mouseover(function() {
+    $("#os5").stop().fadeOut(20); 
+    $("#hw5").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw5").stop().fadeOut(20); 
+    $("#os5").stop().fadeIn(20);  
+  });
+  
+$( "#gc6" )
+  .mouseover(function() {
+    $("#os6").stop().fadeOut(20); 
+    $("#hw6").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw6").stop().fadeOut(20); 
+    $("#os6").stop().fadeIn(20);  
+  });
+
+$( "#gc7" )
+  .mouseover(function() {
+    $("#os7").stop().fadeOut(20); 
+    $("#hw7").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw7").stop().fadeOut(20); 
+    $("#os7").stop().fadeIn(20);  
+  });
+
+$( "#gc8" )
+  .mouseover(function() {
+    $("#os8").stop().fadeOut(20); 
+    $("#hw8").stop().fadeIn(20);  
+  })
+  .mouseout(function() {
+    $("#hw8").stop().fadeOut(20); 
+    $("#os8").stop().fadeIn(20);  
+  });
+
+// ---------------- last trades ----------------
 $( "#lt1" )
   .mouseover(function() {
     $("#ltbt1").stop().fadeOut(20); 
