b68b3c2da1534530470c972a78e77e4caa6ef412
Nyolczas <nyolczas@gmail.com>
2018-04-28 03:08:25 +0200

    hozam

--- Java/FileReadWrite/src/csvFileRead.java
+++ Java/FileReadWrite/src/csvFileRead.java
@@ -7,12 +7,31 @@ public class csvFileRead {
         try {
             List<String> lines = Files.readAllLines(Paths.get("AUDUSD_M5_Zoom.csv"));
 
+            // ellenőrző logolás
             for (String line : lines) {
                 String[] result = line.split(";");
                 for(String s : result) {
                     System.out.print(s + " ### ");
                 }
                 System.out.println();
+            }
+            System.out.println();
+
+            // darabszám kiderítése:
+            int db =lines.size();
+            System.out.println("A lista " + db + " darab bejegyzést tartalmazott.");
+            System.out.println();
+
+
+            // a profitok hozzáadása a hozam tömbhöz
+            float[] hozam = new float[db];
+            float temphozam = 0;
+
+            for(int i = 1; i < db; i++){
+                String[] result = lines.get(i).split(";");
+                temphozam += Float.parseFloat(result[6]);
+                hozam[i] = temphozam;
+                System.out.println(result[6] + " hozam: " + hozam[i]);
             }
 
         } catch (Exception e) {
---
+++ Java/FileReadWrite/src/ticket.java
@@ -0,0 +1,12 @@
+public class ticket {
+    private float profit;
+
+
+    public float getProfit() {
+        return profit;
+    }
+
+    public void setProfit(float profit) {
+        this.profit = profit;
+    }
+}
