bd806f65c7d05484a88c0a80ecf7d87b62aaa708
Nyolczas <nyolczas@gmail.com>
2019-01-04 00:25:49 +0100

    instrumentum szűrés

--- htmlStatement19/js/manual.js
+++ htmlStatement19/js/manual.js
@@ -8,34 +8,22 @@ setTableData(0, manuTrades.length);
 function init() {
   document.getElementById("datefrom").value = "2018-10-20";
   document.getElementById("dateto").value = formatDate(new Date());
-}
-
-//--- dátum formátum
-function formatDate(date) {
-  var d = new Date(date),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [year, month, day].join('-');
+  document.getElementById("instrumentSelector").value = "összes";
 }
 
 //---------------------- táblázat szűrése
-
 document.querySelector('.filter').addEventListener('click', function filter() {
 
-  //--- kezdő dátum megkeresése
+  //--- szűrési feltételek beolvasása
   var dateFrom = new Date(document.getElementById("datefrom").value);
   var dateTo = new Date(document.getElementById("dateto").value);
+  var sym = document.getElementById("instrumentSelector").value;
 
   var start = searchCounter(dateFrom);
   var end = searchCounter(dateTo);
 
   //alert("start: "+ start + ", end: " + end);
-  setTableData(start, end);
+  setTableData(start, end, sym);
 
 });
 
@@ -50,7 +38,7 @@ searchCounter = function (date) {
 }
 
 //---------------------- táblázat összeállítása
-function setTableData(start, end) {
+function setTableData(start, end, sym) {
   var tabbleData = {
     top: '<div style="overflow-x:auto;"></div>\
   <table class="table table-sm">',
@@ -72,64 +60,86 @@ function setTableData(start, end) {
   <th>%</th>\
   <th>Comment</th>\
   </tr>',
-    rows: rowFill(start, end),
+    rows: rowFill(start, end, sym),
     butt: '</table>\
   </div>'
   }
 
-  //---------------------- kiírás
+  //---------------------- táblázat fejlécének kiírása
   document.getElementById("table").innerHTML = tabbleData.top +
     tabbleData.head + tabbleData.rows + tabbleData.butt;
 }
 
-function rowFill(start, end) {
+function rowFill(start, end, sym = "összes") {
 
   var res = "";
   var symbols = [];
   var x = document.getElementById("instrumentSelector");
-  for(var i = x.length; i > 0; i--){
+  for (var i = x.length; i > 0; i--) {
     x.remove(i);
-    }
+  }
+  if (sym != "összes") {
+    var option = document.createElement("option");
+    option.text = sym;
+    x.add(option);
+  }
   for (var i = start; i < end; i++) {
     //--- select option hozzáadása a szimbólum szűréshez
-    if (!symbols.includes(manuTrades[i].Item)) {
+    if (!symbols.includes(manuTrades[i].Item) && sym == "összes") {
       symbols.push(manuTrades[i].Item);
       var option = document.createElement("option");
       option.text = manuTrades[i].Item;
       x.add(option);
     }
     //--- táblázat sorok feltöltése
-    var opt = dateConvertBjuti(manuTrades[i].OpenTime);
-    var clt = dateConvertBjuti(manuTrades[i].CloseTime);
-    res += '<tr>\
-    <td>' + manuTrades[i].Ticket + '</td>\
-    <td>' + opt + '</td>\
-    <td>' + manuTrades[i].Type + '</td>\
-    <td>' + manuTrades[i].Size + '</td>\
-    <td>' + manuTrades[i].Item + '</td>\
-    <td>' + manuTrades[i].OpenPrice + '</td>\
-    <td>' + manuTrades[i].SL + '</td>\
-    <td>' + manuTrades[i].TP + '</td>\
-    <td>' + clt + '</td>\
-    <td>' + manuTrades[i].ClosePrice + '</td>\
-    <td>' + manuTrades[i].Comission + '</td>\
-    <td>' + manuTrades[i].Swap + '</td>\
-    <td>' + manuTrades[i].Profit + '</td>\
-    <td>' + manuTrades[i].NetProfit + '</td>\
-    <td>' + manuTrades[i].PercProfit + '%</td>\
-    <td>' + manuTrades[i].Comment + '</td>\
-  </tr>';
+    if (sym == "összes" || manuTrades[i].Item == sym) {
+      var opt = dateConvertBjuti(manuTrades[i].OpenTime);
+      var clt = dateConvertBjuti(manuTrades[i].CloseTime);
+      res += '<tr>\
+      <td>' + manuTrades[i].Ticket + '</td>\
+      <td>' + opt + '</td>\
+      <td>' + manuTrades[i].Type + '</td>\
+      <td>' + manuTrades[i].Size + '</td>\
+      <td>' + manuTrades[i].Item + '</td>\
+      <td>' + manuTrades[i].OpenPrice + '</td>\
+      <td>' + manuTrades[i].SL + '</td>\
+      <td>' + manuTrades[i].TP + '</td>\
+      <td>' + clt + '</td>\
+      <td>' + manuTrades[i].ClosePrice + '</td>\
+      <td>' + manuTrades[i].Comission + '</td>\
+      <td>' + manuTrades[i].Swap + '</td>\
+      <td>' + manuTrades[i].Profit + '</td>\
+      <td>' + manuTrades[i].NetProfit + '</td>\
+      <td>' + manuTrades[i].PercProfit + '%</td>\
+      <td>' + manuTrades[i].Comment + '</td>\
+    </tr>';
+    }
   }
-  
 
+  x.value = sym;
   //--- táblázat sorok visszaküldése
   return (res);
 }
 
+//====================== Helpers ======================
+//--- date bjuti
 function dateConvertBjuti(date) {
   var d = new Date(date);
   var res = d.toISOString();
   res = res.replace("T", " ");
   res = res.replace(".000Z", " ");
   return res;
+}
+
+//--- dátum formátum
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
 }
