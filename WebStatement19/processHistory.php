27942da1e55315fa703cfd855bc859e8de62e9f9
Nyolczas <nyolczas@gmail.com>
2019-05-04 22:31:28 +0200

    Update processHistory.php

--- WebStatement19/processHistory.php
+++ WebStatement19/processHistory.php
@@ -20,16 +20,82 @@ function processBalanceChart()
     global $fee;
     global $dividend;
 
+    $tempDepo = 0;
+    $tempBalance = 0;
+    $tempManual = 0;
+    $tempRobot = 0;
+    $tempFee = 0;
+    $tempDiv = 0;
+
     global $dayArr;
     global $historyArray;
 
-    foreach ($dayArr as $day) {
-        $histCnt = 0;
-        // if($day == substr($historyArray[cnt][8], 0, 10)){
+    $histCnt = 0;
+    $histDay = substr($historyArray[1][8], 0, 10);
+    $first = true;
 
-        // }
+    for ($day = 0; $day < count($dayArr); $day++) {
+        global $tempDepo;
+        global $tempBalance;
+        global $tempManual;
+        global $tempRobot;
+        global $tempFee;
+        global $tempDiv;
+
+        if (substr($historyArray[$histCnt][8], 0, 10) > $dayArr[$day]) {
+            if ($first) {
+                array_push($fullDepo, 0);
+                array_push($balance, 0);
+                array_push($manual, 0);
+                array_push($robot, 0);
+                array_push($fee, 0);
+                array_push($dividend, 0);
+
+                $first = false;
+            } else {
+                while (substr($historyArray[$histCnt][8], 0, 10) != $dayArr[$day]) {
+                    array_push($fullDepo, $tempDepo);
+                    array_push($balance, $tempBalance);
+                    array_push($manual, $tempManual);
+                    array_push($robot, $tempRobot);
+                    array_push($fee, $tempFee);
+                    array_push($dividend, $tempDiv);
+                    $histCnt++;
+                }
+                $profit = $historyArray[$histCnt][10] + $historyArray[$histCnt][11] + $historyArray[$histCnt][12];
+                $tempBalance += $profit;
+                if ($historyArray[$histCnt][0] == "Depo") {
+                    $tempDepo += $profit;
+                } elseif ($historyArray[$histCnt][0] == "Fee") {
+                    $tempFee += $profit;
+                } elseif ($historyArray[$histCnt][0] == "Adj") {
+                    $tempDiv += $profit;
+                } elseif ($historyArray[$histCnt][0] == "manual") {
+                    $tempManual += $profit;
+                } else {
+                    $tempRobot += $profit;
+                }
+                $histCnt++;
+            }
+        } else {
+            while (substr($historyArray[$histCnt][8], 0, 10) < $dayArr[$day + 1]) {
+                $profit = $historyArray[$histCnt][10] + $historyArray[$histCnt][11] + $historyArray[$histCnt][12];
+                $balance[$histCnt] = $balance[$histCnt - 1] + $profit;
+                if ($historyArray[$histCnt][0] == "Depo") {
+                    $fullDepo[$histCnt] = $fullDepo[$histCnt - 1] + $profit;
+                } elseif ($historyArray[$histCnt][0] == "Fee") {
+                    $fee[$histCnt] = $fee[$histCnt - 1] + $profit;
+                } elseif ($historyArray[$histCnt][0] == "Adj") {
+                    $dividend[$histCnt] = $dividend[$histCnt - 1] + $profit;
+                } elseif ($historyArray[$histCnt][0] == "manual") {
+                    $manual[$histCnt] = $manual[$histCnt - 1] + $profit;
+                } else {
+                    $robot[$histCnt] = $robot[$histCnt - 1] + $profit;
+                }
+                $histCnt++;
+            }
+        }
     }
-
 }
 //-------------------------------------------------------------------------------------
 // napi vizsgálat idő intervallumának betöltése a dayArr tömbbe
@@ -72,8 +138,10 @@ function processHistory($file)
         fclose($handle);
     }
 
-    var_dump($historyArray);
+    //var_dump($historyArray);
 }
 
-dayRange("2019-03-05");
 processHistory('mt4data/FullHistory_27019217.csv');
+//echo substr($historyArray[1][1],0,10);
+dayRange(substr($historyArray[1][1], 0, 10));
+processBalanceChart();
