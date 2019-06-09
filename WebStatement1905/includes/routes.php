e764393cc532b98c8dfc7c474ec35477b50916ab
Nyolczas <nyolczas@gmail.com>
2019-06-09 22:07:01 +0200

    process Robots

---
+++ WebStatement1905/includes/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_ENV["USER"];
+
+$dir = getcwd();
+//--- Paths
+$admLivePath = $dir . '\Admiral2\MQL4\Files\csvStatement_27019217';
+$admDemoPath = $dir . '\Admiral2\MQL4\Files\csvStatement_23129454';
+$xmLivePath = $dir . '\XM1\MQL4\Files\csvStatement_19061180';
+$xmDemoPath = $dir . '\XM2\MQL4\Files\csvStatement_9107935';
+
